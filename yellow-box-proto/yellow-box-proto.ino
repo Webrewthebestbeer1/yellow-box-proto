42804ed385bd3de3d79572be07bc91897da46066
Håkon <hakon.gimse@knowit.no>
2019-01-09 20:42:29 +0100

    rotary encoder

--- yellow-box-proto/yellow-box-proto.ino
+++ yellow-box-proto/yellow-box-proto.ino
@@ -20,6 +20,15 @@ Adafruit_MAX31865 max = Adafruit_MAX31865(45);
  */
 LedControl lc = LedControl(12,11,10,1);
 
+static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
+static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
+volatile byte aFlag = 0; // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
+volatile byte bFlag = 0; // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
+int encoderPos = 200; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
+int oldEncPos = 200; //stores the last encoder position value so we can compare to the current reading and see if it has changed (so we know when to print to the serial monitor)
+volatile byte reading = 0; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
+
+
 void setup() {
   max.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
   /*
@@ -31,7 +40,12 @@ void setup() {
   lc.setIntensity(0,8);
   /* and clear the display */
   lc.clearDisplay(0);
-
+  
+  pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  attachInterrupt(0, PinA, RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(1, PinB, RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+  Serial.begin(9600); // start the serial monitor link
   Serial.begin(115200);
 }
 
@@ -90,7 +104,38 @@ void displayTemperature(float temp) {
   lc.setChar(0, 0, 'c', false);
 }
 
+void PinA() {
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; // read all eight pin values then strip away all but pinA and pinB's values
+  if (reading == B00001100 && aFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos --; //decrement the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00000100) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
+
+void PinB() {
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; //read all eight pin values then strip away all but pinA and pinB's values
+  if (reading == B00001100 && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos ++; //increment the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00001000) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
+
 void loop() {
+  if (oldEncPos != encoderPos) {
+    if (encoderPos < 0) {
+      encoderPos = 0;
+    }
+    Serial.println(encoderPos / 10.0);
+    oldEncPos = encoderPos;
+  }
   uint16_t rtd = max.readRTD();
 
   float ratio = rtd;
@@ -102,7 +147,7 @@ void loop() {
   } else if (temp >= 100.0) {
     displayBoil();
   } else {
-    displayTemperature(temp);  
+    displayTemperature(encoderPos / 10.0);  
   }
   
   Serial.print("Temperature = "); Serial.println(temp);
